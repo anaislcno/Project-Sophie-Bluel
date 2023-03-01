3d85d463e958b8082f89012e0963729a0b2ea6de
An <anaislcno@gmail.com>
2023-03-01 20:23:02 +0100

    add gallery on the modal

--- FrontEnd/works.js
+++ FrontEnd/works.js
@@ -74,7 +74,6 @@ buttonHotel.addEventListener("click", function () {
 });
 
 // Highlight sur le filtre sélectionné
-
 let buttonFilters = document.getElementById("filter");
 let buttonHighlight = buttonFilters.getElementsByClassName("btn");
 
@@ -154,3 +153,37 @@ function enableEditingMode() {
 }
 
 enableEditingMode();
+
+// Grid modale
+
+// const reponse = await fetch("http://localhost:5678/api/works");
+// const works = await reponse.json();
+
+// Fonction qui génère toute la page web
+
+function generateWorksGrid(works) {
+  // Boucle pour tous les éléments
+  for (let i = 0; i < works.length; i++) {
+    const article = works[i];
+    // Récupération de l'élément du DOM qui accueillera les fiches
+    const divEditGallery = document.querySelector("#edit-gallery");
+    // Création d’une balise dédiée à un projet
+    const worksElement = document.createElement("figure");
+
+    // Création des balises
+    const imageUrlElement = document.createElement("img");
+    imageUrlElement.src = article.imageUrl;
+    imageUrlElement.alt = article.imageUrl;
+    const titleElement = document.createElement("figcaption");
+    titleElement.innerText = "éditer";
+
+    // On rattache la balise article a la div
+    divEditGallery.appendChild(worksElement);
+    // On rattache l’image à la figure
+    worksElement.appendChild(imageUrlElement);
+    worksElement.appendChild(titleElement);
+  }
+}
+
+// Premier affichage de la page
+generateWorksGrid(works);
