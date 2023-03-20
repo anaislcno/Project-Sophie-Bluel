f6a93a29452b625d0a3b9e56911e0f933e253b3d
An <anaislcno@gmail.com>
2023-03-20 16:47:18 +0100

    edit refreshing for the adding form

--- FrontEnd/works.js
+++ FrontEnd/works.js
@@ -1,7 +1,7 @@
 const reponse = await fetch("http://localhost:5678/api/works");
 const works = await reponse.json();
 
-// fonction qui ajoute une image a la gallery en donnant un titre et une image url
+// F° pour ajouter une img avec un titre et une imageUrl
 function addToGallery(title, imageUrl, imageId) {
   // Gallery Principale
 
@@ -72,7 +72,8 @@ function addToGalleryGrid(imageUrl, imageId) {
 }
 
 function removeFromGallery(imageId) {
-  document.getElementById(imageId).remove(); // supprimer ds gallery imageId + grid imageId
+  // Supprimer gallery imageId + grid imageId
+  document.getElementById(imageId).remove();
 }
 // Fonction qui génère toute la page web
 
@@ -81,23 +82,6 @@ function generateWorks(works) {
   for (let i = 0; i < works.length; i++) {
     const article = works[i];
     addToGallery(article.title, article.imageUrl, article.id);
-    // // Récupération de l'élément du DOM qui accueillera les fiches
-    // const divGallery = document.querySelector(".gallery");
-    // // Création d’une balise dédiée à un projet
-    // const worksElement = document.createElement("figure");
-
-    // // Création des balises
-    // const imageUrlElement = document.createElement("img");
-    // imageUrlElement.src = article.imageUrl;
-    // imageUrlElement.alt = article.imageUrl;
-    // const titleElement = document.createElement("figcaption");
-    // titleElement.innerText = article.title;
-
-    // // On rattache la balise article a la div
-    // divGallery.appendChild(worksElement);
-    // // On rattache l’image à la figure
-    // worksElement.appendChild(imageUrlElement);
-    // worksElement.appendChild(titleElement);
   }
 }
 
@@ -169,6 +153,7 @@ const buttonModal = document.getElementById("btn-modal");
 const closeModal = document.getElementById("close");
 const modalAdd = document.getElementById("modal-add");
 const modalGallery = document.getElementById("modal-gallery");
+const buttonValidate = document.getElementById("valid");
 
 //  Apparition de la modale qd on clique s/ le bouton
 buttonModal.addEventListener("click", function () {
@@ -179,6 +164,13 @@ buttonModal.addEventListener("click", function () {
 
 //La modale se ferme si on clique sur la croix
 closeModal.addEventListener("click", function () {
+  modal.style.display = "none";
+  overlay.style.display = "none";
+  modalGallery.style.display = "block";
+});
+
+// Fermeture lors de l'ajout d'un projet
+buttonValidate.addEventListener("click", function () {
   modal.style.display = "none";
   overlay.style.display = "none";
   modalGallery.style.display = "block";
@@ -208,6 +200,11 @@ const buttonNewProject = document.getElementById("cta-edit");
 buttonNewProject.addEventListener("click", function () {
   modalGallery.style.display = "none";
   modalAdd.style.display = "block";
+  resetForm.reset();
+  document.getElementById("preview").src = "#";
+  document.getElementById("preview").alt = "";
+  const displayPreview = document.getElementById("hide");
+  displayPreview.style.display = "block";
 });
 
 // Retour à la galerie modale
@@ -260,54 +257,6 @@ function enableEditingMode() {
 }
 
 enableEditingMode();
-
-// Grid modale
-// Fonction qui génère
-
-// function generateWorksGrid(works) {
-//   // Boucle pour tous les éléments
-//   for (let i = 0; i < works.length; i++) {
-//     const article = works[i];
-//     // Récupération de l'élément du DOM qui accueillera les fiches
-//     const divEditGallery = document.querySelector("#edit-gallery");
-//     // Création d’une balise dédiée à un projet
-//     const worksElement = document.createElement("figure");
-//     worksElement.classList.add("figure-element");
-
-//     // Création des balises
-//     const imageUrlElement = document.createElement("img");
-//     imageUrlElement.src = article.imageUrl;
-//     imageUrlElement.alt = article.imageUrl;
-//     const titleElement = document.createElement("figcaption");
-//     titleElement.innerText = "éditer";
-
-//     const buttonDelete = document.createElement("i");
-//     buttonDelete.classList.add("fa-solid", "fa-trash-can", "btn-delete");
-//     // On rattache la balise article a la div
-//     divEditGallery.appendChild(worksElement);
-//     // On rattache l’image à la figure
-
-//     worksElement.appendChild(imageUrlElement);
-//     worksElement.appendChild(titleElement);
-//     worksElement.appendChild(buttonDelete);
-
-//     // Boutton de suppression
-//     buttonDelete.addEventListener("click", function (event) {
-//       event.preventDefault();
-//       const token = window.localStorage.getItem("accessToken");
-//       const deleteMethod = {
-//         method: "DELETE",
-//         headers: {
-//           authorization: "Bearer " + token,
-//         },
-//       };
-//       fetch("http://localhost:5678/api/works/" + article.id, deleteMethod);
-//     });
-//   }
-// }
-
-// // Affichage de la page
-// generateWorksGrid(works);
 
 // Visualition img formulaire
 
